da3d2ad86e04fcf0ed00265f6bac67b4af616671
koudaix700 <xiaopan20062007@163.com>
2017-04-30 22:32:42 +0800

    Create sw.js

---
+++ test/sw/demo4/sw.js
@@ -0,0 +1,18 @@
+self.addEventListener('push', event => {
+  event.waitUntil(
+    // Process the event and display a notification.
+    console.log("Process the event and display a notification")
+    self.registration.showNotification("Hey!")
+  );
+});
+
+self.addEventListener('notificationclick', event => {  
+  // Do something with the event  
+  console.log("Do something with the event")
+  event.notification.close();  
+});
+
+self.addEventListener('notificationclose', event => {  
+  // Do something with the event 
+  console.log("Do something with the event")
+});
