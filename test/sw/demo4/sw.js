e7a89fcd9578ae65be028496b0092b652fe9439c
koudaix700 <xiaopan20062007@163.com>
2017-04-30 22:37:37 +0800

    Update sw.js

--- test/sw/demo4/sw.js
+++ test/sw/demo4/sw.js
@@ -1,14 +1,12 @@
 self.addEventListener('push', event => {
   event.waitUntil(
     // Process the event and display a notification.
-    console.log("Process the event and display a notification")
     self.registration.showNotification("Hey!")
   );
 });
 
 self.addEventListener('notificationclick', event => {  
   // Do something with the event  
-  console.log("Do something with the event")
   event.notification.close();  
 });
 
