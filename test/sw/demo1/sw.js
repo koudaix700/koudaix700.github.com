463ee97c6964698d26e3cf3231a28fce9a252653
koudaix700 <xiaopan20062007@163.com>
2017-04-30 21:49:38 +0800

    Update sw.js

--- test/sw/demo1/sw.js
+++ test/sw/demo1/sw.js
@@ -1,7 +1,7 @@
 console.log("success");
 console.log(this);
-self.addEventListener('fetch',event => {
-    event.respondWith(
+self.addEventListener('fetch',function(event) {
+    return event.respondWith(
         new Response("World");
     );
 })
