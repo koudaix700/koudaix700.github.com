61e41a838b6c3d9853fa7a9210a1a4a2b58e2053
koudaix700 <xiaopan20062007@163.com>
2017-04-30 21:46:09 +0800

    Update sw.js

--- test/sw/demo1/sw.js
+++ test/sw/demo1/sw.js
@@ -1,1 +1,9 @@
-console.log("success")
+<script type="text/javascript">
+console.log("success");
+console.log(this);
+self.addEventListener('fetch',event => {
+    event.respondWith(
+        new Response("World");
+    );
+})
+</script>
